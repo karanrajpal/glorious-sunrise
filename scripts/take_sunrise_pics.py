a2b37a7870aaecfd3c89a7bb21560b2098ed6db9
Karan Rajpal <karanrajpal001@gmail.com>
2020-03-26 01:36:11 -0400

    New options, sending email, compressing GIF

--- scripts/take_sunrise_pics.py
+++ scripts/take_sunrise_pics.py
@@ -7,27 +7,36 @@ from notifier import send_email
 
 # Setup argument parsing and use defaults
 parser = argparse.ArgumentParser()
-parser.add_argument("--number_of_images", "-n", default=10, type=int)   # integer
-parser.add_argument("--capture_interval", "-i", default=5, type=int)    # seconds
-parser.add_argument("--gif_image_delay", "-g", default=30, type=int)    # milliseconds
+parser.add_argument("--number_of_images", "-n", default=150, type=int)   # integer
+parser.add_argument("--capture_interval", "-i", default=30, type=int)    # seconds
+parser.add_argument("--gif_image_delay", "-g", default=10, type=int)    # milliseconds
 parser.add_argument("--compress", "-c", default=False, type=bool)    # milliseconds
+parser.add_argument("--output_file_name", "-o", default='animation', type=str)    # milliseconds
 args = parser.parse_args()
 
 media_path = '/home/pi/glorious-sunrise/media'
+output_file_path = '{}/{}.gif'.format(media_path, args.output_file_name)
+
 print('Clearing out previous media')
 system('rm -rf {}'.format(media_path))
 system('mkdir {}'.format(media_path))
 
 camera = PiCamera()
 if (args.compress):
-    camera.resolution = (720, 480)
+    camera.resolution = (640, 480)
 
 for i in range(args.number_of_images):
-    sleep(args.capture_interval)
-    camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
     print('Taking picture numer {}'.format(i))
+    camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
+    sleep(args.capture_interval)
 
 print('Done taking pics. Generating GIF')
-system('convert -delay {} -loop 0 {}/image*.jpg {}/animation.gif'.format(args.gif_image_delay, media_path, media_path))
+system('convert -delay {} -loop 0 {}/image*.jpg {}'.format(args.gif_image_delay, media_path, output_file_path))
+system('gifsicle --colors 256 --scale 0.8 {} -o {}'.format(output_file_path, output_file_path))
 print('done converting GIFs')
-send_email(os.environ['SUNRISE_RECIPIENT'], 'It''s a glorious sunrise!', 'Check out this amazing animation of the sunrise that you missed while you were asleep', ['/home/pi/glorious-sunrise/media/animation.gif'])
+send_email(
+    os.environ['SUNRISE_RECIPIENT'],
+    'It''s a glorious sunrise!',
+    'Check out this amazing animation of the sunrise that you missed while you were asleep',
+    [output_file_path]
+)

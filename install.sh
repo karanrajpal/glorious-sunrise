3b72aa5913d1c5a37e8e472f1c00c7cf60bfc3b5
Karan Rajpal <karanrajpal001@gmail.com>
2020-04-01 09:32:50 -0400

    New gif conversion options and earlier time

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ git clone https://github.com/karanrajpal/glorious-sunrise.git
 mkdir -p glorious-sunrise/media
 
 # Write to crontab to run every morning
-(crontab -l 2>/dev/null; echo "40 6 * * * python /home/pi/glorious-sunrise/scripts/take_sunrise_pics.py -n=150 -i=30 -g=10 -c=TRUE") | crontab -
+(crontab -l 2>/dev/null; echo "10 6 * * * python /home/pi/glorious-sunrise/scripts/take_sunrise_pics.py -n=150 -i=40 -g=10 -c=TRUE") | crontab -
--- scripts/take_sunrise_pics.py
+++ scripts/take_sunrise_pics.py
@@ -32,7 +32,7 @@ for i in range(args.number_of_images):
 
 print('Done taking pics. Generating GIF')
 system('convert -delay {} -loop 0 {}/image*.jpg {}'.format(args.gif_image_delay, media_path, output_file_path))
-system('gifsicle --colors 256 --scale 0.8 {} -o {}'.format(output_file_path, output_file_path))
+system('gifsicle -O3 --scale 0.8 {} -o {}'.format(output_file_path, output_file_path))
 print('done converting GIFs')
 send_email(
     os.environ['SUNRISE_RECIPIENT'],

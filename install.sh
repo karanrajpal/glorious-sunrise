741e410193373e2398eff9fc83ffc40e45afe012
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-24 12:07:47 -0400

    Installing n8n and picamera

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Please note: This install script is roughly cobbled together for a new raspberry pi and might fail on re-install.
+# Just re-install stuff as needed from below
 set +eux
 sudo apt-get update
 sudo apt-get install imagemagick -y
@@ -12,12 +15,29 @@ sudo apt-get install gifsicle
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
+rm get-pip.py
+
 cd scripts
 pip install picamera
 cd ..
 
 # Make media folder which doesn't exist
-mkdir -p glorious-sunrise/media
+mkdir -p media
+
+
+# Installing node
+sudo apt install nodejs -y
+sudo apt install npm -y
+
+# We also want to prevent permission errors, so we need following operations (credits to https://stackoverflow.com/questions/49894620/npx-command-not-found):
+mkdir ~/.npm-global # create folder where npm will install packages
+npm config set prefix '~/.npm-global' # configure npm
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile # add folder to path in user profile
+source ~/.profile # reload current user profile
+
+# Installing n8n
+npm install n8n -g
+n8n
 
 # Write to crontab to run every morning
 (crontab -l 2>/dev/null; echo "10 6 * * * python /home/pi/glorious-sunrise/scripts/take_sunrise_pics.py -n=150 -i=40 -g=10 -c=TRUE") | crontab -

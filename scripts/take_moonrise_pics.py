3321d9c9dd3c9bc177e6407527ce171583914f47
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-26 22:56:29 -0400

    first sleep

--- scripts/take_moonrise_pics.py
+++ scripts/take_moonrise_pics.py
@@ -5,9 +5,9 @@ camera = PiCamera()
 
 media_path = '/home/pi/glorious-sunrise/media'
 
-# camera.exposure_mode = 'spotlight'
 
 for i in range(1, 10):
+    sleep(5)
+    camera.exposure_mode = 'spotlight'
     print('Taking picture numer {}'.format(i))
     camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
-    sleep(5)

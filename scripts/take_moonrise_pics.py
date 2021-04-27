712dc3bff6214a8faebed4701ce040e5a2b1f2cf
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-26 22:36:46 -0400

    um

---
+++ scripts/take_moonrise_pics.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+media_path = '/home/pi/glorious-sunrise/media'
+
+for i in range(1, 10):
+    print('Taking picture numer {}'.format(i))
+    camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
+    sleep(5)

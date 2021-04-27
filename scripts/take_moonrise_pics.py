9db256f079fc84a0fb8d21e0a683f571dbe17100
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-26 23:30:25 -0400

    night

--- scripts/take_moonrise_pics.py
+++ scripts/take_moonrise_pics.py
@@ -8,10 +8,10 @@ media_path = '/home/pi/glorious-sunrise/media'
 
 shortlisted_exposure_modes = ['off', 'auto', 'night', 'nightpreview', 'backlight', 'spotlight', 'sports', 'snow', 'beach', 'verylong', 'fixedfps', 'antishake', 'fireworks']
 for i in range(1, 10):
-    for exposure_string in shortlisted_exposure_modes:
+    # for exposure_string in shortlisted_exposure_modes:
         sleep(5)
         print(camera.digital_gain)
         print(camera.analog_gain)
-        camera.exposure_mode = exposure_string
+        camera.exposure_mode = 'night'
         camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
         print('Taking picture numer {}'.format(i))

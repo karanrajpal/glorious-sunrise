a9298d87b9c04238c551650996fd24a96d78446c
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-26 23:14:44 -0400

    hopefully this

--- scripts/take_moonrise_pics.py
+++ scripts/take_moonrise_pics.py
@@ -6,9 +6,10 @@ camera = PiCamera()
 media_path = '/home/pi/glorious-sunrise/media'
 
 
+shortlisted_exposure_modes = ['off', 'auto', 'night', 'nightpreview', 'backlight', 'spotlight', 'sports', 'snow', 'beach', 'verylong', 'fixedfps', 'antishake', 'fireworks']
 for i in range(1, 10):
-    sleep(5)
-    camera.exposure_mode = 'spotlight'
-    camera.awb_mode = 'auto'
-    print('Taking picture numer {}'.format(i))
-    camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
+    for exposure_string in shortlisted_exposure_modes:
+        sleep(5)
+        camera.exposure_mode = exposure_string
+        camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
+        print('Taking picture numer {}'.format(i))

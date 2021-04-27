a1765b56e69bfc9bc2f7a086c21f9735ea1bbe48
Karan Rajpal <karanrajpal001@gmail.com>
2021-04-26 22:42:45 -0400

    exposure is magic?

--- scripts/take_moonrise_pics.py
+++ scripts/take_moonrise_pics.py
@@ -5,6 +5,8 @@ camera = PiCamera()
 
 media_path = '/home/pi/glorious-sunrise/media'
 
+camera.exposure_mode = 'spotlight'
+
 for i in range(1, 10):
     print('Taking picture numer {}'.format(i))
     camera.capture('{0}/image{1:04d}.jpg'.format(media_path, i))
